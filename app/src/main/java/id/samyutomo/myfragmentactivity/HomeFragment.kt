85afaba7a44e3269b497de367736e98243a7aeba
samyutomo <samyutomo@gmail.com>
2023-01-10 03:11:25 +0700

    final stage part 1 - basic fragment on frame layout

---
+++ app/src/main/java/id/samyutomo/myfragmentactivity/HomeFragment.kt
@@ -0,0 +1,29 @@
+package id.samyutomo.myfragmentactivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnCategory:Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
+    }
+    
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+}
