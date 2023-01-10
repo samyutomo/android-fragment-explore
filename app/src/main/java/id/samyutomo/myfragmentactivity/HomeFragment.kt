205ea80d7fb01fd94dc8ba0f88d31ca6427ad828
samyutomo <samyutomo@gmail.com>
2023-01-10 11:18:46 +0700

    final stage - implemented parentFragmentManager, replace, addToBackStack

--- app/src/main/java/id/samyutomo/myfragmentactivity/HomeFragment.kt
+++ app/src/main/java/id/samyutomo/myfragmentactivity/HomeFragment.kt
@@ -19,11 +19,19 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnCategory:Button = view.findViewById(R.id.btn_category)
+        val btnCategory: Button = view.findViewById(R.id.btn_category)
         btnCategory.setOnClickListener(this)
     }
-    
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_category) {
+            val mCategoryFragment = CategoryFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
